119f384f05825e907a345871b58744cc29dbc5b1
ThanhDanh2k1 <kieudanh.github@gmail.com>
2023-12-25 13:26:45 +0700

    project Old

---
+++ serve/models/cart.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose")
+
+const accountSchema = new mongoose.Schema ({
+    nameProduct: {
+        type: String,
+        required: true
+    },
+    idProduct: {
+        type: String,
+        required: true
+    },
+    imageProduct: {
+        type: String,
+        required: true
+    },
+    priceProduct: {
+        type: Number,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model("cart", accountSchema)
